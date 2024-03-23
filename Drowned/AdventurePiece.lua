a3a91828c11ea7dab5cc6fdc3067d6658456ff2e
VampNtr <79720867+malfume@users.noreply.github.com>
2024-03-23 12:43:08 -0500

    Update AdventurePiece.lua

--- Drowned/AdventurePiece.lua
+++ Drowned/AdventurePiece.lua
@@ -1,11 +1,11 @@
--- Get the expected GameID
+-- Define the expected GameID
 local expectedGameID = 9731516308
 
 -- Get the current game ID
 local currentGameID = game.GameId
 
--- Check if the current game ID matches the expected game ID
-function init()
+-- Define the init function
+local function init()
     if currentGameID == expectedGameID then
         -- Print a message indicating that the script has loaded
         print("Drowned Hub Has Loaded for Game ID " .. expectedGameID)
