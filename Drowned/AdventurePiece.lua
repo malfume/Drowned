8cab78988c4572ecf65bfe2b454fdd29d3952973
VampNtr <79720867+malfume@users.noreply.github.com>
2024-03-23 13:03:06 -0500

    Update AdventurePiece.lua

--- Drowned/AdventurePiece.lua
+++ Drowned/AdventurePiece.lua
@@ -3,9 +3,9 @@ local currentGameID = game.GameId
 
 local function init()
     if currentGameID == expectedGameID then
-        print("Behold, mortal! The Drowned Hub has been summoned for Game ID " .. expectedGameID .. ". Prepare for an abyssal journey!")
+        print("test for Game ID " .. expectedGameID .. ". j!")
     else
-        print("Ah, a twist of fate! The Drowned Hub has loaded, but for a different Game ID: " .. currentGameID .. ". Fear not, for the darkness still beckons!")
+        print("test different Game ID: " .. currentGameID .. ". Fns!")
     end
 end
 
