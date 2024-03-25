9d5e0f96deae5d651e86663c9d2a488129e7e5b6
VampNtr <79720867+malfume@users.noreply.github.com>
2024-03-24 21:50:36 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,12 +1,28 @@
-local expectedGameID = 11535401491
-local currentGameID = game.GameId
+-- Test.lua
+local Test = {}
 
-local function init()
-    if currentGameID == expectedGameID then
-        print("Correct id for Game ID " .. expectedGameID .. ". j!")
-    else
-        print("different Game ID: " .. currentGameID .. ". Fns!")
-    end
+-- This is a helper function that you want to test
+function Test.helperFunction(a, b)
+  return a + b
 end
 
-return init
+-- This is a test function that checks if the helper function returns the correct result
+function Test.testHelperFunction()
+  local result = Test.helperFunction(1, 2)
+  assert(result == 3, "Expected 1 + 2 to equal 3, but got " .. tostring(result))
+end
+
+-- This is a test function that checks if the helper function throws an error when given invalid arguments
+function Test.testHelperFunctionInvalidArguments()
+  local status, err = pcall(Test.helperFunction, "1", 2)
+  assert(not status and err:find("bad argument #1"), "Expected an error when passing a string as the first argument, but got " .. tostring(err))
+end
+
+-- This function runs all the tests
+function Test.runTests()
+  Test.testHelperFunction()
+  Test.testHelperFunctionInvalidArguments()
+end
+
+-- Run the tests
+Test.runTests()
